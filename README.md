3da22aac877252d9ab81db99994839f000a7e235
wswu <wswu24@gmail.com>
2023-01-04 00:55:57 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Analyzing Acknowledgments in NLP Proceedings
+
+Code to accompany our paper, published in NLP+CSS 2022.
+
+To replicate the results, first download the PDFs using `download.sh` (6.6 GB).
+
+Analysis is contained in the `Acknowledgments.ipynb` notebook.
---
+++ src/download.sh
@@ -0,0 +1,50 @@
+# download EMNLP proceedings, about 4 GB
+mkdir -p ../data/emnlp
+wget -O ../data/emnlp/2021.pdf https://aclanthology.org/2021.emnlp-main.pdf
+wget -O ../data/emnlp/2020.pdf https://aclanthology.org/2020.emnlp-main.pdf
+wget -O ../data/emnlp/2019.pdf https://aclanthology.org/D19-1.pdf
+wget -O ../data/emnlp/2018.pdf https://aclanthology.org/D18-1.pdf
+wget -O ../data/emnlp/2017.pdf https://aclanthology.org/D17-1.pdf
+wget -O ../data/emnlp/2016.pdf https://aclanthology.org/D16-1.pdf
+wget -O ../data/emnlp/2015.pdf https://aclanthology.org/D15-1.pdf
+wget -O ../data/emnlp/2014.pdf https://aclanthology.org/D14-1.pdf
+wget -O ../data/emnlp/2013.pdf https://aclanthology.org/D13-1.pdf
+wget -O ../data/emnlp/2012.pdf https://aclanthology.org/D12-1.pdf
+wget -O ../data/emnlp/2011.pdf https://aclanthology.org/D11-1.pdf
+wget -O ../data/emnlp/2010.pdf https://aclanthology.org/D10-1.pdf
+wget -O ../data/emnlp/2009.pdf https://aclanthology.org/D09-1.pdf
+wget -O ../data/emnlp/2008.pdf https://aclanthology.org/D08-1.pdf
+wget -O ../data/emnlp/2007.pdf https://aclanthology.org/D07-1.pdf
+wget -O ../data/emnlp/2006.pdf https://aclanthology.org/W06-16.pdf
+wget -O ../data/emnlp/2005.pdf https://aclanthology.org/H05-1.pdf
+
+# download ACL proceedings, about 2.6 GB
+mkdir -p ../data/acl
+wget -O ../data/acl/2021-long.pdf https://aclanthology.org/2021.acl-long.pdf
+wget -O ../data/acl/2021-short.pdf https://aclanthology.org/2021.acl-short.pdf
+wget -O ../data/acl/2020.pdf https://aclanthology.org/2020.acl-main.pdf
+wget -O ../data/acl/2019.pdf https://aclanthology.org/P19-1.pdf
+wget -O ../data/acl/2018-long.pdf https://aclanthology.org/P18-1.pdf
+wget -O ../data/acl/2018-short.pdf https://aclanthology.org/P18-2.pdf
+wget -O ../data/acl/2017-long.pdf https://aclanthology.org/P17-1.pdf
+wget -O ../data/acl/2017-short.pdf https://aclanthology.org/P17-2.pdf
+wget -O ../data/acl/2016-long.pdf https://aclanthology.org/P16-1.pdf
+wget -O ../data/acl/2016-short.pdf https://aclanthology.org/P16-2.pdf
+wget -O ../data/acl/2015-long.pdf https://aclanthology.org/P15-1.pdf
+wget -O ../data/acl/2015-short.pdf https://aclanthology.org/P15-2.pdf
+wget -O ../data/acl/2014-long.pdf https://aclanthology.org/P14-1.pdf
+wget -O ../data/acl/2014-short.pdf https://aclanthology.org/P14-2.pdf
+wget -O ../data/acl/2013-long.pdf https://aclanthology.org/P13-1.pdf
+wget -O ../data/acl/2013-short.pdf https://aclanthology.org/P13-2.pdf
+wget -O ../data/acl/2012-long.pdf https://aclanthology.org/P12-1.pdf
+wget -O ../data/acl/2012-short.pdf https://aclanthology.org/P12-2.pdf
+wget -O ../data/acl/2011-long.pdf https://aclanthology.org/P11-1.pdf
+wget -O ../data/acl/2011-short.pdf https://aclanthology.org/P11-2.pdf
+wget -O ../data/acl/2010-long.pdf https://aclanthology.org/P10-1.pdf
+wget -O ../data/acl/2010-short.pdf https://aclanthology.org/P10-2.pdf
+wget -O ../data/acl/2009-long.pdf https://aclanthology.org/P09-1.pdf
+wget -O ../data/acl/2009-short.pdf https://aclanthology.org/P09-2.pdf
+wget -O ../data/acl/2008.pdf https://aclanthology.org/P08-1.pdf
+wget -O ../data/acl/2007.pdf https://aclanthology.org/P07-1.pdf
+wget -O ../data/acl/2006.pdf https://aclanthology.org/P06-1.pdf
+wget -O ../data/acl/2005.pdf https://aclanthology.org/P05-1.pdf
